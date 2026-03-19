0518b7960d0d0427a614539d4ed268d9ff1fcafb
ToF- <cthibauttof@gmail.com>
2026-03-19 19:47:40 +0100

    matches

--- lib/mastermind.ml
+++ lib/mastermind.ml
@@ -1,1 +1,8 @@
-let matches guess secret = 2
+let matches guess secret = 
+    let rec matches_acc n guess secret = 
+        if n = 0
+        then 0
+        else match (guess mod 10) = (secret mod 10) with
+        true -> 1 + matches_acc (n - 1) (guess / 10) (secret / 10)
+        | false ->    matches_acc (n - 1) (guess / 10) (secret / 10)
+    in matches_acc 4 guess secret
--- test/test_mastermind.ml
+++ test/test_mastermind.ml
@@ -2,7 +2,9 @@ open OUnit2
 open Mastermind
 
 let tests = "mastermind" >::: [
-    "dummy" >:: (fun _ -> assert_equal 2 (matches 1234 1664));
+    "same color on same position count as matches" >:: (fun _ -> assert_equal 2 (matches 1234 1664));
+    "not any color matching counts as zero matches" >:: (fun _ -> assert_equal 0 (matches 1122 3456));
+    "all colors matching counts as four matches" >:: (fun _ -> assert_equal 4 (matches 1645 1645));
 ]
 
 let () =
